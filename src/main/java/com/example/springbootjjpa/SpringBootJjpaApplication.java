335197abde78831b61f16a0771378fd455dcc92e
BeobSik <tjswl756@naver.com>
2021-08-23 16:47:08 +0900

    feat: add PostgreSQL

--- src/main/java/com/example/springbootjjpa/SpringBootJjpaApplication.java
+++ src/main/java/com/example/springbootjjpa/SpringBootJjpaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJjpaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJjpaApplication.class, args);
     }
