a451d72249d3d92c2a87cf8855c037d64b3bd1e6
BeobSik <tjswl756@naver.com>
2021-08-23 16:41:11 +0900

    init

---
+++ src/main/java/com/example/springbootjjpa/SpringBootJjpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJjpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootjjpa/SpringBootJjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
